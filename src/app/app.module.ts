4c32bfe4cd3c042bd7b10dc94923ad2430652d23
Zaki Shaikh <zaki-shaikh@outlook.com>
2021-02-07 13:01:05 +0530

    Removed table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TableComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
